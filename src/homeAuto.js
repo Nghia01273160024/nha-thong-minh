81fc354a88fb6d0bc8bb4a902b219b1b7daf418c
Tran Nghia <choidalbo365@gmail.com>
2023-03-22 03:52:57 +0700

    hihi

---
+++ src/homeAuto.js
@@ -0,0 +1,22 @@
+import './homeStyle.css'
+import React, { useState } from 'react';
+import { WiDaySunny, WiRain } from "react-icons/wi";
+const HomeAuto = ({ onToggle }) => {
+    const [isRain, setIsRain] = useState(false);
+
+    const handleToggle = () => {
+    const newState = !isRain;
+    setIsRain(newState);
+    onToggle(newState); 
+  }
+    return(
+        <div className={`switch-container ${isRain ? "rain" : "sunny"}`}>
+        <label className="switch">
+          <input type="checkbox" checked={isRain} onChange={handleToggle} />
+          <span className="slider round"></span>
+        </label>
+      </div>
+    )
+} 
+
+export default HomeAuto;
