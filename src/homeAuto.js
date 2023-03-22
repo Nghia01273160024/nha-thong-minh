0894fad1673807d105d7ea85f14eedd174cd8e7d
Tran Nghia <choidalbo365@gmail.com>
2023-03-23 01:57:36 +0700

    update

--- src/homeAuto.js
+++ src/homeAuto.js
@@ -1,13 +1,13 @@
 import './homeStyle.css'
 import React, { useState } from 'react';
-import { WiDaySunny, WiRain } from "react-icons/wi";
+import Box from '@mui/material/Box';
+import SvgIcon from '@mui/material/SvgIcon';
 const HomeAuto = ({ onToggle }) => {
     const [isRain, setIsRain] = useState(false);
 
     const handleToggle = () => {
-    const newState = !isRain;
-    setIsRain(newState);
-    onToggle(newState); 
+    setIsRain(!isRain);
+    onToggle(!isRain); 
   }
     return(
         <div className={`switch-container ${isRain ? "rain" : "sunny"}`}>
@@ -15,11 +15,20 @@ const HomeAuto = ({ onToggle }) => {
           <input type="checkbox" checked={isRain} onChange={handleToggle} />
           <span className="slider round"></span>
         </label>
-       <div>
-        <a>hihi</a>
-        <a>haha</a>
-       </div>
+        <label className="switch">
+          <input type="checkbox" checked={isRain} onChange={handleToggle} />
+          <span className="slider round"></span>
+        </label>
+          <Box
+           className='home-icon'
+    
+    >
+      <SvgIcon sx={{ fontSize: 100}}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+    </Box>
       </div>
+      
     )
 } 
 
